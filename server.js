3913bdfe3cb43eb88e0deeb5647abca4d2507924
Marcin <marcincoles@yahoo.com>
2017-06-20 22:24:03 +1000

    more triangles

--- server.js
+++ server.js
@@ -76,6 +76,11 @@ app.get('/', function (req, res) {
   }
 });
 
+app.get('/tri', function (req, res) {
+
+  res.render('tri.html')
+});
+
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
